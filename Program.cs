304229fca34740ea78baf66d0bc0774be25e1be7
Paul Betts <paul@paulbetts.org>
2011-12-12 15:05:05 -0800

    Make sure GitPad.exe doesn't run elevated

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -20,6 +21,13 @@ namespace Gitpad
     {
         public static int Main(string[] args)
         {
+            if (IsProcessElevated())
+            {
+                MessageBox.Show("Run this application as a normal user (not as Elevated Administrator)",
+                                "App is Elevated", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+
             int ret = 0;
             if (args.Length == 0)
             {
@@ -126,5 +134,225 @@ namespace Gitpad
 
             return ret.ToString();
         }
+
+        public static unsafe bool IsProcessElevated()
+        {
+            IntPtr tokenHandle;
+            if (!NativeMethods.OpenProcessToken(NativeMethods.GetCurrentProcess(), NativeMethods.TOKEN_QUERY, out tokenHandle))
+            {
+                throw new Exception("OpenProcessToken failed");
+            }
+
+            try
+            {
+                TOKEN_ELEVATION_TYPE elevationType;
+                uint dontcare;
+                if (!NativeMethods.GetTokenInformation(tokenHandle, TOKEN_INFORMATION_CLASS.TokenElevationType, out elevationType, (uint)sizeof(TOKEN_ELEVATION_TYPE), out dontcare))
+                {
+                    throw new Exception("GetTokenInformation failed");
+                }
+
+                Console.WriteLine(elevationType);
+                return (elevationType == TOKEN_ELEVATION_TYPE.TokenElevationTypeFull);
+            }
+            finally
+            {
+                NativeMethods.CloseHandle(tokenHandle);
+            }
+        }
+    }
+
+    public enum TOKEN_INFORMATION_CLASS
+    {
+        /// <summary>
+        /// The buffer receives a TOKEN_USER structure that contains the user account of the token.
+        /// </summary>
+        TokenUser = 1,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_GROUPS structure that contains the group accounts associated with the token.
+        /// </summary>
+        TokenGroups,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_PRIVILEGES structure that contains the privileges of the token.
+        /// </summary>
+        TokenPrivileges,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_OWNER structure that contains the default owner security identifier (SID) for newly created objects.
+        /// </summary>
+        TokenOwner,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_PRIMARY_GROUP structure that contains the default primary group SID for newly created objects.
+        /// </summary>
+        TokenPrimaryGroup,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_DEFAULT_DACL structure that contains the default DACL for newly created objects.
+        /// </summary>
+        TokenDefaultDacl,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_SOURCE structure that contains the source of the token. TOKEN_QUERY_SOURCE access is needed to retrieve this information.
+        /// </summary>
+        TokenSource,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_TYPE value that indicates whether the token is a primary or impersonation token.
+        /// </summary>
+        TokenType,
+
+        /// <summary>
+        /// The buffer receives a SECURITY_IMPERSONATION_LEVEL value that indicates the impersonation level of the token. If the access token is not an impersonation token, the function fails.
+        /// </summary>
+        TokenImpersonationLevel,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_STATISTICS structure that contains various token statistics.
+        /// </summary>
+        TokenStatistics,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_GROUPS structure that contains the list of restricting SIDs in a restricted token.
+        /// </summary>
+        TokenRestrictedSids,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that indicates the Terminal Services session identifier that is associated with the token. 
+        /// </summary>
+        TokenSessionId,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_GROUPS_AND_PRIVILEGES structure that contains the user SID, the group accounts, the restricted SIDs, and the authentication ID associated with the token.
+        /// </summary>
+        TokenGroupsAndPrivileges,
+
+        /// <summary>
+        /// Reserved.
+        /// </summary>
+        TokenSessionReference,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that is nonzero if the token includes the SANDBOX_INERT flag.
+        /// </summary>
+        TokenSandBoxInert,
+
+        /// <summary>
+        /// Reserved.
+        /// </summary>
+        TokenAuditPolicy,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_ORIGIN value. 
+        /// </summary>
+        TokenOrigin,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_ELEVATION_TYPE value that specifies the elevation level of the token.
+        /// </summary>
+        TokenElevationType,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_LINKED_TOKEN structure that contains a handle to another token that is linked to this token.
+        /// </summary>
+        TokenLinkedToken,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_ELEVATION structure that specifies whether the token is elevated.
+        /// </summary>
+        TokenElevation,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that is nonzero if the token has ever been filtered.
+        /// </summary>
+        TokenHasRestrictions,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_ACCESS_INFORMATION structure that specifies security information contained in the token.
+        /// </summary>
+        TokenAccessInformation,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that is nonzero if virtualization is allowed for the token.
+        /// </summary>
+        TokenVirtualizationAllowed,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that is nonzero if virtualization is enabled for the token.
+        /// </summary>
+        TokenVirtualizationEnabled,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_MANDATORY_LABEL structure that specifies the token's integrity level. 
+        /// </summary>
+        TokenIntegrityLevel,
+
+        /// <summary>
+        /// The buffer receives a DWORD value that is nonzero if the token has the UIAccess flag set.
+        /// </summary>
+        TokenUIAccess,
+
+        /// <summary>
+        /// The buffer receives a TOKEN_MANDATORY_POLICY structure that specifies the token's mandatory integrity policy.
+        /// </summary>
+        TokenMandatoryPolicy,
+
+        /// <summary>
+        /// The buffer receives the token's logon security identifier (SID).
+        /// </summary>
+        TokenLogonSid,
+
+        /// <summary>
+        /// The maximum value for this enumeration
+        /// </summary>
+        MaxTokenInfoClass
+    }
+
+    public enum TOKEN_ELEVATION_TYPE
+    {
+        TokenElevationTypeDefault = 1,
+        TokenElevationTypeFull,
+        TokenElevationTypeLimited
+    }
+
+    public static class NativeMethods
+    {
+        public const UInt32 STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public const UInt32 STANDARD_RIGHTS_READ = 0x00020000;
+        public const UInt32 TOKEN_ASSIGN_PRIMARY = 0x0001;
+        public const UInt32 TOKEN_DUPLICATE = 0x0002;
+        public const UInt32 TOKEN_IMPERSONATE = 0x0004;
+        public const UInt32 TOKEN_QUERY = 0x0008;
+        public const UInt32 TOKEN_QUERY_SOURCE = 0x0010;
+        public const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        public const UInt32 TOKEN_ADJUST_GROUPS = 0x0040;
+        public const UInt32 TOKEN_ADJUST_DEFAULT = 0x0080;
+        public const UInt32 TOKEN_ADJUST_SESSIONID = 0x0100;
+        public const UInt32 TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+        public const UInt32 TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+            TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+            TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+            TOKEN_ADJUST_SESSIONID);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool GetTokenInformation(
+            IntPtr TokenHandle,
+            TOKEN_INFORMATION_CLASS TokenInformationClass,
+            out TOKEN_ELEVATION_TYPE TokenInformation,
+            uint TokenInformationLength,
+            out uint ReturnLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
     }
 }
